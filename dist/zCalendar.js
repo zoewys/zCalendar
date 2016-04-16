e7465696e13b7f0319bf9c15bbd951355bb9889f
zoe <zoew.ys@outlook.com>
2016-04-16 18:38:37 +0800

    init

---
+++ dist/zCalendar.js
@@ -0,0 +1,24 @@
+/**
+ * Created by zoe on 4/16/16.
+ */
+
+if (typeof jQuery === "undefined") { throw new Error("Requires jQuery") }
+
+(function($){
+
+    $.fn.zCalendar = function(){
+        
+        var settings = {
+
+            
+
+        };
+
+        // init zCalendar
+        var init = function(itemSelector,options){
+
+        }
+    }
+
+})(jQuery);
+
